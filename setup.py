7bfdeb2bc804c89dea73c2c4bc9a604f7f0ca6a3
wkd8176 <37936802+wkd8176@users.noreply.github.com>
2019-03-27 11:01:16 +0900

    version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='wideq_kr',
-    version='0.0.6',
+    version='0.0.8',
     description='LG SmartThinQ API client for Korean',
     author='wkd8176',
     author_email='wkd8176@gmail.com',
