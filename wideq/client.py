696f65e122502f4638b4018726b6948d753a9aef
pifou <nicolas_gras@hotmail.com>
2020-10-26 23:26:32 +0100

    Update client.py

--- wideq/client.py
+++ wideq/client.py
@@ -265,7 +265,7 @@ class DeviceInfo(object):
     DeviceType.DRYER : DryerDevice,
     DeviceType.WASHER : WasherDevice,
     """
-    mapping : dict[DeviceType, Device] = {}
+    mapping: dict[DeviceType, Device()] = {}
 
     def __init__(self, data: Dict[str, Any]) -> None:
         self.data = data
