b42985fc482d250ccbc4f2ddc2c3e1e1dd5f4a41
Grant Holliday <gholliday@users.noreply.github.com>
2020-11-28 17:10:04 +1100

    Don't crash when instant power isn't supported by unit

--- wideq/ac.py
+++ wideq/ac.py
@@ -4,7 +4,7 @@ import enum
 
 from .client import Device
 from .util import lookup_enum
-from .core import FailedRequestError
+from .core import FailedRequestError, InvalidRequestError
 
 
 class ACJetMode(enum.Enum):
@@ -315,14 +315,20 @@ class ACDevice(Device):
     def get_outdoor_power(self):
         """Get instant power usage in watts of the outdoor unit"""
 
-        value = self._get_config('OutTotalInstantPower')
-        return value['OutTotalInstantPower']
+        try:
+            value = self._get_config('OutTotalInstantPower')
+            return value['OutTotalInstantPower']
+        except InvalidRequestError:
+            return 0  # Device does not support instant power usage of the outdoor unit
 
     def get_power(self):
         """Get the instant power usage in watts of the whole unit"""
 
-        value = self._get_config('InOutInstantPower')
-        return value['InOutInstantPower']
+        try:
+            value = self._get_config('InOutInstantPower')
+            return value['InOutInstantPower']
+        except InvalidRequestError:
+            return 0  # Device does not support instant power usage of the whole unit
 
     def get_light(self):
         """Get a Boolean indicating whether the display light is on."""
